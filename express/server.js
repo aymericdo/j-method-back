c82ac5744b497f65f9304bd7b5ccd216f7b8e45e
Aymeric Dominique <aymeric.dominique@gmail.com>
2020-08-22 12:27:22 +0200

    retest

--- express/server.js
+++ express/server.js
@@ -24,11 +24,11 @@ const vapidKeys = {
 };
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.FRONT_ADDRESS); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", process.env.FRONT_ADDRESS); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 webpush.setVapidDetails(
   'mailto:aymeric.dominique@gmail.com',
