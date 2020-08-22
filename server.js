9b6d03327a0a7f548d629b88704fcc2bb96dd523
Aymeric Dominique <aymeric.dominique@gmail.com>
2020-08-22 13:23:26 +0200

    test

--- server.js
+++ server.js
@@ -21,25 +21,6 @@ const vapidKeys = {
   privateKey:"Wev87nA92dTFhnr9HFTTDDS7zE-4GMtuxFHS8NokVCU",
 };
 
-const allowedOrigins = [
-  'http://localhost:4200',
-  'https://nifty-einstein-f120b7.netlify.app',
-];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      let msg = 'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
-
 webpush.setVapidDetails(
   'mailto:aymeric.dominique@gmail.com',
   vapidKeys.publicKey,
